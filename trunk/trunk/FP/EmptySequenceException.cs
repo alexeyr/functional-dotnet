689008c1d08a4f2eb670b7779e68bd1ed8ba8190
alexey.v.romanov <alexey.v.romanov@3a54de37-aa49-0410-b22f-8bbb064ca0b5>
2008-04-05 18:28:44 +0000

    Initial import

---
+++ trunk/trunk/FP/EmptySequenceException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FP
+{
+    /// <summary>
+    /// Thrown by the methods which require their argument to be non-empty
+    /// when called on an empty sequence.
+    /// </summary>
+    public class EmptySequenceException : InvalidOperationException
+    {
+        public EmptySequenceException(string message, Exception innerException) : base(message, innerException)
+        {}
+
+        public EmptySequenceException(string message) : base(message)
+        {}
+    }
+}
---
+++ trunk/trunk/FPTests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+using FP;
+
+namespace FPTests
+{
+    public class EnumerableTests
+    {
+        [Fact]
+        public void TailShouldThrowOnEmptySeq()
+        {
+            var emptyList = Enumerable.Empty<int>();
+            Assert.Throws(typeof(EmptySequenceException), () => emptyList.Tail());
+        }
+    }
+}
