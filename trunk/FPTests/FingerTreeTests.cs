e0c8e3f5e1d4ab8141e1ec7bc029507c7ea62f22
alexey.v.romanov <alexey.v.romanov@3a54de37-aa49-0410-b22f-8bbb064ca0b5>
2008-07-29 13:39:33 +0000

    Tests for OrderedSequence pass.

--- trunk/FPTests/FingerTreeTests.cs
+++ trunk/FPTests/FingerTreeTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using FP;
 using FP.Collections.Immutable;
@@ -6,14 +7,14 @@ using Xunit;
 using XunitExtensions;
 
 namespace FPTests {
-    public class SequenceTests {
+    public class RASequenceTests {
         private readonly RandomAccessSequence<int> _empty = RandomAccessSequence.Empty<int>();
         private readonly int[] _testData;
         private readonly RandomAccessSequence<int> _seq;
         private const int N = 10000;
         private readonly Random _rnd = new Random();
 
-        public SequenceTests() {
+        public RASequenceTests() {
             _testData = new int[N];
             for (int i = 0; i < N; i++)
                 _testData[i] = _rnd.Next();
@@ -116,6 +117,76 @@ namespace FPTests {
             Assert.Equal(0, seq[i2]);
             Assert.Equal(0, seq[i1]);
             Assert2.SequenceEqual(_testData, seq.RemoveAt(i3).RemoveAt(i2 - 1).RemoveAt(i1 - 2));
+        }
+    }
+
+    public class OrdSequenceTests {
+        private readonly OrderedSequence<int, int> _empty = OrderedSequence.Empty(int.MinValue);
+        private readonly int[] _testData = new int[N];
+        private readonly int[] _testDataSorted = new int[N];
+        private readonly OrderedSequence<int, int> _seq;
+        private const int N = 1000;
+        private readonly Random _rnd = new Random();
+
+        private static IEnumerable<T> Stutter<T>(IEnumerable<T> ts) {
+            foreach (var t in ts) {
+                yield return t;
+                yield return t;
+            }
+        }
+
+        private static IEnumerable<T> EveryNth<T>(IEnumerable<T> ts, int n) {
+            int i = 0;
+            foreach (var t in ts) {
+                i++;
+                if (i == n) {
+                    yield return t;
+                    i = 0;
+                }
+            }
+        }
+
+        public OrdSequenceTests() {
+            for (int i = 0; i < N; i++)
+                _testData[i] = _rnd.Next(N);
+            _testData.CopyTo(_testDataSorted, 0);
+            Array.Sort(_testDataSorted);
+            _seq = OrderedSequence.FromEnumerable(_testData, int.MinValue);
+        }
+
+        [Fact]
+        public void EnumerateWorks() {
+            Assert2.SequenceEqual(_testDataSorted, _seq);
+        }
+
+        [Fact]
+        public void FromOEnumWorks() {
+            Assert2.SequenceEqual(_testDataSorted, OrderedSequence.FromOrderedEnumerable(_testDataSorted, int.MinValue));
+        }
+
+        [Fact]
+        public void MergeWorks() {
+            for (int count = N / 4; count < N; count += N / 4) {
+                var enum1 = _testData.Take(count);
+                var enum2 = _testData.Skip(count);
+                var seq1 = OrderedSequence.FromEnumerable(enum1, int.MinValue);
+                var seq2 = OrderedSequence.FromEnumerable(enum2, int.MinValue);
+                Assert2.SequenceEqual(_testDataSorted, seq1.Union(seq2));
+                Assert2.SequenceEqual(_testDataSorted, seq2.Union(seq1));
+            }
+        }
+
+        [Fact]
+        public void IntersectWorks() {
+            var enum1 = _testData.Distinct().ToArray();
+            var enum2 = EveryNth(enum1, 2);
+            var enum3 = EveryNth(enum1, 3);
+            var enum6 = EveryNth(enum1, 6);
+            var seq2 = OrderedSequence.FromEnumerable(enum2, int.MinValue);
+            var seq3 = OrderedSequence.FromEnumerable(enum3, int.MinValue);
+            var seq6 = OrderedSequence.FromEnumerable(enum6, int.MinValue);
+            Assert2.SequenceEqual(seq6, seq2.Intersect(seq3));
+            Assert2.SequenceEqual(seq6, seq3.Intersect(seq2));
         }
     }
 }
