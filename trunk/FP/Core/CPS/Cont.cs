3123986b52283d60a126ba937101446d37ce7585
alexey.v.romanov <alexey.v.romanov@3a54de37-aa49-0410-b22f-8bbb064ca0b5>
2008-12-06 14:07:14 +0000

    Readd Future, since Lazy is needed. Add Combinators. Start CPS actions.

---
+++ trunk/FP/Core/CPS/Cont.cs
@@ -0,0 +1,34 @@
+﻿/*
+* Cont.cs is part of functional-dotnet project
+* 
+* Copyright (c) 2008 Alexey Romanov
+* All rights reserved.
+*
+* This source file is available under The New BSD License.
+* See license.txt file for more information.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FP.Core.CPS {
+    ///<summary>
+    /// A continuation.
+    ///</summary>
+    ///<typeparam name="T"></typeparam>
+    public class Cont<T> {
+        public readonly Action<T> Ok;
+        public readonly Action<Exception> Fail;
+        public Cont(Action<T> ok, Action<Exception> fail) {
+            Ok = ok;
+            Fail = fail;
+        }
+    }
+}
---
+++ trunk/FP/Future/CyclicFutureException.cs
@@ -0,0 +1,25 @@
+﻿#region License
+/*
+* CyclicFutureException.cs is part of functional-dotnet project
+* 
+* Copyright (c) 2008 Alexey Romanov
+* All rights reserved.
+*
+* This source file is available under The New BSD License.
+* See license.txt file for more information.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+*/
+#endregion
+
+using System;
+
+namespace FP.Future {
+    /// <summary>
+    /// Thrown if the result of a future is the same future.
+    /// </summary>
+    public class CyclicFutureException : InvalidOperationException {}
+}
