c9b8e493415885608a96534177c9c45077716e41
alexey.v.romanov <alexey.v.romanov@3a54de37-aa49-0410-b22f-8bbb064ca0b5>
2008-12-28 09:54:03 +0000

    Added a small validation framework from Brewster's blog.

---
+++ trunk/FP.Validation/Validation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace FP.Validation {
+    /// <summary>
+    /// Represents validation of preconditions.
+    /// </summary>
+    /// <remarks>Cannot be constructed directly; call <see cref="Requires.That"/> to
+    /// begin validation and extension methods from <see cref="ValidationExtensions"/> to
+    /// check conditions.</remarks>
+    /// <seealso cref="ValidationExtensions"/>
+    public class Validation {
+        private readonly List<Exception> _exceptions;
+
+        internal Exception[] Exceptions {
+            get { return _exceptions.ToArray(); }
+        }
+
+        internal Validation AddException(Exception ex) {
+            lock (_exceptions) {
+                _exceptions.Add(ex);
+            }
+
+            return this;
+        }
+
+        internal Validation() {
+            _exceptions = new List<Exception>(1); // optimize for only having 1 exception
+        }
+
+        ~Validation() {
+            throw new ValidationException("validation.Check() was not called!", Exceptions);
+        }
+
+        [DebuggerNonUserCode]
+        internal virtual void Throw() {
+            Exception exception = _exceptions.Count == 1
+                          ? _exceptions[0]
+                          : new MultiException(Exceptions);
+            throw new ValidationException(exception);
+        }
+    }
+}
