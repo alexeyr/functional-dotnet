f94d7986e64c5deac3e8f5881a9bb6a08d9b9da7
alexey.v.romanov <alexey.v.romanov@3a54de37-aa49-0410-b22f-8bbb064ca0b5>
2009-01-15 13:24:39 +0000

    Merged CharSequenceRope with SubStringRope.

--- FP.Validation/ValidationExtensions.cs
+++ FP.Validation/ValidationExtensions.cs
@@ -56,13 +56,12 @@ namespace FP.Validation {
         /// Validate that the specified parameter is in range between two bounds (both
         /// bounds are valid).
         /// </summary>
-        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
         /// <param name="validation">The validation.</param>
         /// <param name="lowerBound">The lower bound (inclusive).</param>
         /// <param name="upperBound">The upper bound (inclusive).</param>
         /// <param name="value">The value of the parameter.</param>
         /// <param name="paramName">The name of the parameter.</param>
-        /// <returns></returns>
         public static Validation IsInRange<T>(this Validation validation, T lowerBound, T upperBound, T value, string paramName)
         where T : IComparable<T> {
             if (value.CompareTo(lowerBound) >= 0 && value.CompareTo(upperBound) <= 0)
@@ -76,45 +75,15 @@ namespace FP.Validation {
         } // IsInRange(validation, lowerBound, upperBound, value, paramName)
 
         /// <summary>
-        /// Validate that the specified parameter has a value and is in range between two
-        /// bounds (both bounds are valid).
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="validation">The validation.</param>
-        /// <param name="lowerBound">The lower bound (inclusive).</param>
-        /// <param name="upperBound">The upper bound (inclusive).</param>
-        /// <param name="nullableValue">The value of the parameter.</param>
-        /// <param name="paramName">The name of the parameter.</param>
-        /// <returns></returns>
-        public static Validation IsInRange<T>(this Validation validation, T lowerBound, T upperBound, T? nullableValue, string paramName)
-        where T : struct, IComparable<T> {
-            if (!nullableValue.HasValue) {
-                return validation.AddException(
-                    new ArgumentNullException(
-                        paramName,
-                        string.Format("must be between {0} and {1}, but was null", lowerBound, upperBound)));
-            }
-            T value = nullableValue.Value;
-            if (value.CompareTo(lowerBound) >= 0 && value.CompareTo(upperBound) <= 0)
-                return validation;
-            return
-                validation.AddException(
-                    new ArgumentOutOfRangeException(
-                        paramName,
-                        string.Format("must be between {0} and {1}, but was {2}", lowerBound, upperBound, value)));
-        } // IsInRange(validation, lowerBound, upperBound, nullableValue, paramName)
-
-        /// <summary>
         /// Validate that the specified parameter is in range between two bounds (lower
         /// bound is valid, but upper bound is not).
         /// </summary>
-        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
         /// <param name="validation">The validation.</param>
         /// <param name="lowerBound">The lower bound (inclusive).</param>
         /// <param name="upperBound">The upper bound (exclusive).</param>
         /// <param name="value">The value of the parameter.</param>
         /// <param name="paramName">The name of the parameter.</param>
-        /// <returns></returns>
         public static Validation IsInUpperExclusiveRange<T>(this Validation validation, T lowerBound, T upperBound, T value, string paramName)
         where T : IComparable<T> {
             if (value.CompareTo(lowerBound) >= 0 && value.CompareTo(upperBound) < 0)
@@ -127,45 +96,15 @@ namespace FP.Validation {
         } // IsInRange(validation, lowerBound, upperBound, value, paramName)
 
         /// <summary>
-        /// Validate that the specified parameter is in range between two bounds (lower
-        /// bound is valid, but upper bound is not).
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="validation">The validation.</param>
-        /// <param name="lowerBound">The lower bound (inclusive).</param>
-        /// <param name="upperBound">The upper bound (exclusive).</param>
-        /// <param name="nullableValue">The value of the parameter.</param>
-        /// <param name="paramName">The name of the parameter.</param>
-        /// <returns></returns>
-        public static Validation IsInUpperExclusiveRange<T>(this Validation validation, T lowerBound, T upperBound, T? nullableValue, string paramName)
-        where T : struct, IComparable<T> {
-            if (!nullableValue.HasValue) {
-                return validation.AddException(
-                    new ArgumentNullException(
-                        paramName,
-                        string.Format("must be between {0} and {1}, but was null", lowerBound, upperBound)));
-            }
-            T value = nullableValue.Value;
-            if (value.CompareTo(lowerBound) >= 0 && value.CompareTo(upperBound) < 0)
-                return validation;
-            return
-                validation.AddException(
-                    new ArgumentOutOfRangeException(
-                        paramName,
-                        string.Format("must be between {0} and {1}, but was {2}", lowerBound, upperBound, value)));
-        } // IsInRange(validation, lowerBound, upperBound, nullableValue, paramName)
-
-        /// <summary>
         /// Validate that the specified parameter is in range between two bounds (neither
         /// bound is valid).
         /// </summary>
-        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
         /// <param name="validation">The validation.</param>
         /// <param name="lowerBound">The lower bound (exclusive).</param>
         /// <param name="upperBound">The upper bound (exclusive).</param>
         /// <param name="value">The value of the parameter.</param>
         /// <param name="paramName">The name of the parameter.</param>
-        /// <returns></returns>
         public static Validation IsInExclusiveRange<T>(this Validation validation, T lowerBound, T upperBound, T value, string paramName)
         where T : IComparable<T> {
             if (value.CompareTo(lowerBound) > 0 && value.CompareTo(upperBound) < 0)
@@ -178,33 +117,80 @@ namespace FP.Validation {
         } // IsInRange(validation, lowerBound, upperBound, value, paramName)
 
         /// <summary>
-        /// Validate that the specified parameter is in range between two bounds (neither
-        /// bound is valid).
+        /// Validate that the specified parameter is not less than the specified lower bound.
         /// </summary>
-        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
         /// <param name="validation">The validation.</param>
-        /// <param name="lowerBound">The lower bound (exclusive).</param>
-        /// <param name="upperBound">The upper bound (exclusive).</param>
-        /// <param name="nullableValue">The value of the parameter.</param>
+        /// <param name="lowerBound">The lower bound (inclusive).</param>
+        /// <param name="value">The value of the parameter.</param>
         /// <param name="paramName">The name of the parameter.</param>
-        /// <returns></returns>
-        public static Validation IsInExclusiveRange<T>(this Validation validation, T lowerBound, T upperBound, T? nullableValue, string paramName)
-        where T : struct, IComparable<T> {
-            if (!nullableValue.HasValue) {
-                return validation.AddException(
-                    new ArgumentNullException(
-                        paramName,
-                        string.Format("must be between {0} and {1}, but was null", lowerBound, upperBound)));
-            }
-            T value = nullableValue.Value;
-            if (value.CompareTo(lowerBound) > 0 && value.CompareTo(upperBound) < 0)
+        public static Validation IsAtLeast<T>(this Validation validation, T lowerBound, T value, string paramName)
+        where T : IComparable<T> {
+            if (value.CompareTo(lowerBound) >= 0)
                 return validation;
             return
                 validation.AddException(
                     new ArgumentOutOfRangeException(
                         paramName,
-                        string.Format("must be between {0} and {1}, but was {2}", lowerBound, upperBound, value)));
-        } // IsInRange(validation, lowerBound, upperBound, nullableValue, paramName)
+                        string.Format("must be at least {0}, but was {1}", lowerBound, value)));
+        } // IsInRange(validation, lowerBound, upperBound, value, paramName)
+
+        /// <summary>
+        /// Validate that the specified parameter is greater than the specified lower bound.
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
+        /// <param name="validation">The validation.</param>
+        /// <param name="lowerBound">The lower bound (exclusive).</param>
+        /// <param name="value">The value of the parameter.</param>
+        /// <param name="paramName">The name of the parameter.</param>
+        public static Validation IsGreaterThan<T>(this Validation validation, T lowerBound, T value, string paramName)
+        where T : IComparable<T> {
+            if (value.CompareTo(lowerBound) > 0)
+                return validation;
+            return
+                validation.AddException(
+                    new ArgumentOutOfRangeException(
+                        paramName,
+                        string.Format("must be greater than {0}, but was {1}", lowerBound, value)));
+        } // IsInRange(validation, lowerBound, upperBound, value, paramName)
+
+        /// <summary>
+        /// Validate that the specified parameter is not greater than the specified lower bound.
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
+        /// <param name="validation">The validation.</param>
+        /// <param name="upperBound">The upper bound (inclusive).</param>
+        /// <param name="value">The value of the parameter.</param>
+        /// <param name="paramName">The name of the parameter.</param>
+        public static Validation IsAtMost<T>(this Validation validation, T upperBound, T value, string paramName)
+        where T : IComparable<T> {
+            if (value.CompareTo(upperBound) <= 0)
+                return validation;
+            return
+                validation.AddException(
+                    new ArgumentOutOfRangeException(
+                        paramName,
+                        string.Format("must be at most {0}, but was {1}", upperBound, value)));
+        } // IsInRange(validation, lowerBound, upperBound, value, paramName)
+
+        /// <summary>
+        /// Validate that the specified parameter is greater than the specified lower bound.
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
+        /// <param name="validation">The validation.</param>
+        /// <param name="upperBound">The upper bound (exclusive).</param>
+        /// <param name="value">The value of the parameter.</param>
+        /// <param name="paramName">The name of the parameter.</param>
+        public static Validation IsLessThan<T>(this Validation validation, T upperBound, T value, string paramName)
+        where T : IComparable<T> {
+            if (value.CompareTo(upperBound) < 0)
+                return validation;
+            return
+                validation.AddException(
+                    new ArgumentOutOfRangeException(
+                        paramName,
+                        string.Format("must be less than {0}, but was {1}", upperBound, value)));
+        } // IsInRange(validation, lowerBound, upperBound, value, paramName)
 
         /// <summary>
         /// Check whether there were any exceptions. Do not forget to call this after
--- FP/Text/ReverseRope.cs
+++ FP/Text/ReverseRope.cs
@@ -24,45 +24,29 @@ namespace FP.Text {
     [Serializable]
     public sealed class ReverseRope<TSequence> : FlatRope where TSequence : IFlatCharSequence {
         private readonly CharSequenceRope<TSequence> _sourceRope;
-        private readonly int _offsetFromEnd;
-        private readonly int _length;
-
-        private int FirstIndex { get { return _sourceRope.Count - _offsetFromEnd - _length; } }
-
-        private int LastIndex { get { return _sourceRope.Count - _offsetFromEnd - 1; } }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ReverseRope{TSequence}"/> class.
         /// </summary>
-        /// <param name="sourceRope">The char sequence.</param>
-        /// <param name="offsetFromEnd">The offset.</param>
-        /// <param name="length">The length.</param>
-        public ReverseRope(CharSequenceRope<TSequence> sourceRope, int offsetFromEnd, int length) {
-            Requires.That.
-                IsIndexAndCountInRange(sourceRope, sourceRope.Count - offsetFromEnd - length, length, "offsetFromEnd", "length").Check();
-
+        /// <param name="sourceRope">The original rope we want to reverse.</param>
+        public ReverseRope(CharSequenceRope<TSequence> sourceRope) {
             _sourceRope = sourceRope;
-            _offsetFromEnd = offsetFromEnd;
-            _length = length;
         }
 
         public override IEnumerator<char> GetEnumerator(int startIndex) {
             Requires.That.IsIndexInRange(this, startIndex, "startIndex").Check();
 
-            int lastIndex = LastIndex;
-            for (int i = 0; i < _length; i++)
-                yield return _sourceRope[lastIndex - i];
+            for (int i = _sourceRope.Count - 1 - startIndex; i >= 0; i--)
+                yield return _sourceRope[i];
         }
 
         public override int Count {
-            get { return _length; }
+            get { return _sourceRope.Count; }
         }
 
         public override char this[int index] {
             get {
-                Requires.That.IsIndexInRange(this, index, "startIndex").Check();
-
-                return _sourceRope[LastIndex - index];
+                return _sourceRope[_sourceRope.Count - 1 - index];
             }
         }
 
@@ -70,31 +54,32 @@ namespace FP.Text {
             Requires.That.IsIndexAndCountInRange(this, sourceIndex, count, "sourceIndex", "count").Check();
 
             // BUG: Possible off-by-one!
-            int firstIndex = _sourceRope.Count - _offsetFromEnd - count;
-            _sourceRope.CopyTo(firstIndex, destination, destinationIndex, count);
+            _sourceRope.CopyTo(_sourceRope.Count - count, destination, destinationIndex, count);
             Array.Reverse(destination, destinationIndex, count);
         }
 
         public override void WriteOut(TextWriter writer, int startIndex, int count) {
-            // _sourceRope.WriteOut(writer, _offsetFromEnd + startIndex, count);
+            writer.Write(this.ToArray(startIndex, count));
         }
 
-        public override Rope SubStringInternal(int startIndex, int count) {
+        internal override Rope SubStringInternal(int startIndex, int count) {
             if (startIndex == 0 && count == Count)
                 return this;
 
             if (count <= MAX_SHORT_SIZE) {
                 // BUG: Possible off-by-one!
-                int firstIndex = _sourceRope.Count - _offsetFromEnd - count;
-                var array = _sourceRope.ToArray(firstIndex, count);
+                var array = _sourceRope.ToArray(_sourceRope.Count - count, count);
                 Array.Reverse(array);
                 return array.ToRope();
             }
-            return new ReverseRope<TSequence>(_sourceRope, _offsetFromEnd + startIndex, count);
+            var charSequence = _sourceRope.CharSequence;
+            int firstIndex = _sourceRope.StartIndex + _sourceRope.Count - startIndex - count;
+            return new ReverseRope<TSequence>(
+                new CharSequenceRope<TSequence>(charSequence, firstIndex, count));
         }
 
         public override Rope Reverse() {
-            return _sourceRope.SubString(_sourceRope.Count - _offsetFromEnd - _length, _length);
+            return _sourceRope;
         }
     }
 }
