fc8e86a11fcda6407ef318bb58b9c15df19503fa
alexey.v.romanov <alexey.v.romanov@3a54de37-aa49-0410-b22f-8bbb064ca0b5>
2009-07-30 20:02:18 +0000

    Improved dictionary interfaces.

---
+++ trunk/FP/Collections/Persistent/ISortedDictionary.cs
@@ -0,0 +1,77 @@
+/*
+* ISortedDictionary.cs is part of functional-dotnet project
+* 
+* Copyright (c) 2009 Alexey Romanov
+* All rights reserved.
+*
+* This source file is available under The New BSD License.
+* See license.txt file for more information.
+* 
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+* CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+* INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+*/
+
+using System;
+using FP.Core;
+
+namespace FP.Collections.Persistent {
+    /// <summary>
+    /// A dictionary which keeps its keys in a sorted order.
+    /// </summary>
+    /// <typeparam name="TKey">The type of the key.</typeparam>
+    /// <typeparam name="TValue">The type of the value.</typeparam>
+    /// <typeparam name="TDictionary">The type of the dictionary.</typeparam>
+    /// <remarks><see cref="IDictionary{TKey,TValue,TDictionary}.Keys"/> and 
+    /// <see cref="IDictionary{TKey,TValue,TDictionary}.Values"/> are guaranteed
+    /// to return the results in ascending order of keys.</remarks>
+    public interface ISortedDictionary<TKey, TValue, TDictionary> :
+        IDictionary<TKey, TValue, TDictionary>
+        where TDictionary : ISortedDictionary<TKey, TValue, TDictionary>
+        where TKey : IComparable<TKey> {
+        /// <summary>
+        /// Retrieves the minimum key, associated value, and the dictionary
+        /// containing all other elements.
+        /// </summary>
+        /// <returns>The tuple of the minimum key, associated value and the
+        /// dictionary containing all other elements, if the dictionary is
+        /// non-empty; <c>None</c> otherwise.</returns>
+        Optional<Tuple<TKey, TValue, TDictionary>> TakeMinKey();
+
+        /// <summary>
+        /// Retrieves the maximum key, associated value, and the dictionary
+        /// containing all other elements.
+        /// </summary>
+        /// <returns>The tuple of the maximum key, associated value and the
+        /// dictionary containing all other elements, if the dictionary is
+        /// non-empty; <c>None</c> otherwise.</returns>
+        Optional<Tuple<TKey, TValue, TDictionary>> TakeMaxKey();
+
+        /// <summary>
+        /// Retrieves the minimum key and the associated value.
+        /// </summary>
+        /// <returns>The tuple of the minimum key, associated value and the
+        /// dictionary containing all other elements, if the dictionary is
+        /// non-empty; <c>None</c> otherwise.</returns>
+        Optional<Tuple<TKey, TValue>> MinKey();
+
+        /// <summary>
+        /// Retrieves the maximum key and the associated value.
+        /// </summary>
+        /// <returns>The tuple of the maximum key, associated value and the
+        /// dictionary containing all other elements, if the dictionary is
+        /// non-empty; <c>None</c> otherwise.</returns>
+        Optional<Tuple<TKey, TValue>> MaxKey();
+
+        /// <summary>
+        /// Splits dictionary on the specified key.
+        /// </summary>
+        /// <param name="key">The key.</param>
+        /// <returns>A tuple, where the first element contains all keys less
+        /// than <paramref name="key"/>, the second element is <c>this[key]</c>,
+        /// and the third element contains all keys greater than 
+        /// <paramref name="key"/></returns>
+        Tuple<TDictionary, Optional<TValue>, TDictionary> Split(TKey key);
+    }
+}
